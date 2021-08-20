175c37cea71321c666cacc3043b2fc463d07525b
Toratomi <tomitodingan18@gmail.com>
2021-08-20 13:28:12 +0800

    perubahan

--- src/pages/Tables/Basic.vue
+++ src/pages/Tables/Basic.vue
@@ -1,7 +1,8 @@
 <template>
   <v-container fluid>
+    
     <div class="tables-basic">
-      <h1 class="page-title mt-10 mb-6">Tables</h1>
+      <h4 class="page-title mt-10 mb-6 black--text">Profil Kelurahan</h4>
       <v-row>
         <!-- <v-col cols="12">
           <v-card class="employee-list mb-1">
@@ -29,16 +30,16 @@
         </v-col> -->
         <v-col cols=12>
           <v-card class="mb-1">
-            <v-card-title class="pa-5 pb-3">
-              <p>Material-UI Table</p>
-              <v-spacer></v-spacer>
-              <v-menu>
+            <v-card-title class="red darken-3">
+            <span class="text-h7 white--text">Identitas Kelurahan</span>
+            <v-spacer></v-spacer>
+            <v-menu>
                 <template v-slot:activator="{ on, attrs }">
                   <v-btn
                     icon
                     v-bind="attrs"
                     v-on="on">
-                    <v-icon color="textColor">mdi-dots-vertical</v-icon>
+                    <v-icon color="white">mdi-dots-vertical</v-icon>
                   </v-btn>
                 </template>
                 <v-list>
@@ -50,14 +51,14 @@
                   </v-list-item>
                 </v-list>
               </v-menu>
-            </v-card-title>
+          </v-card-title>
             <v-simple-table>
                 <template v-slot:default>
                   <thead>
                   <tr>
-                    <th class="text-left pa-6">NAME</th>
+                    <!--<th class="text-left pa-6">NAME</th>
                     <th class="text-left">EMAIL</th>
-                    <!-- <th class="text-left">PRODUCT</th>
+                     <th class="text-left">PRODUCT</th>
                     <th class="text-left">PRICE</th>
                     <th class="text-left">DATE</th>
                     <th class="text-left">CITY</th>
@@ -65,10 +66,41 @@
                   </tr>
                   </thead>
                   <tbody>
-                  <tr v-for="item in mock.materialUITable" :key="item.name">
+                    <tr>
+                  <th scope="row" class="text-h6 black--text">Nama Kelurahan</th>
+                  <td class="text-h6 black--text"><span id="namakelurahan">Neverland 1</span></td>
+                </tr>
+                <tr>
+                  <th scope="row" class="text-h6">Kode Kelurahan</th>
+                  <td class="text-h6"><span id="kodekelurahan">002</span></td>
+                </tr>
+                <tr>
+                  <th scope="row" class="text-h6">Kecamatan</th>
+                  <td class="text-h6"><span id="kecamatan">Ascent</span></td>
+                </tr>
+                <tr>
+                  <th scope="row" class="text-h6">Kabupaten/Kota</th>
+                  <td class="text-h6"><span id="kabupaten">Erangel</span></td>
+                </tr>
+                <tr>
+                  <th scope="row" class="text-h6">Provinsi</th>
+                  <td class="text-h6"><span id="provinsi">Kingdom Inc.</span></td>
+                </tr>
+                <tr>
+                  <th scope="row" class="text-h6">Alamat Kantor Kelurahan</th>
+                  <td class="text-h6"><span id="alamatkantorkelurahan">Jl. Uzumaki 09, Barat Konoha</span></td>
+                </tr>
+                <tr>
+                  <th scope="row" class="text-h6">Telepon Kelurahan</th>
+                  <td class="text-h6"><span id="teleponkelurahan">(0431) 916-086</span></td>
+                </tr>
+                <tr>
+                  <th scope="row" class="text-h6">Email Kelurahan</th>
+                  <td class="text-h6"><span id="emailkelurahan">asdjk@gmail.com</span></td>
+                  <!--tr v-for="item in mock.materialUITable" :key="item.name">
                     <td class="pa-6">{{ item.name }}</td>
                     <td>{{ item.email }}</td>
-                    <!-- <td>{{ item.product }}</td>
+                    < <td>{{ item.product }}</td>
                     <td>{{ item.price }}</td>
                     <td>{{ item.date }}</td>
                     <td>{{ item.city }}</td>
@@ -118,14 +150,26 @@
           >
           </v-img>
           <v-card-title class="red darken-3">
-            <span class="text-h5 white--text">Lurah Kelurahan Karame</span>
+            <span class="text-h8 white--text">Lurah</span>
             <v-spacer></v-spacer>
-            <v-btn
-              dark
-              icon
-            >
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
+            <v-menu>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    icon
+                    v-bind="attrs"
+                    v-on="on">
+                    <v-icon color="white">mdi-dots-vertical</v-icon>
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item
+                    v-for="(item, i) in mock.menu"
+                    :key="i"
+                    @click="() => {}">
+                    <v-list-item-title>{{ item }}</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
           </v-card-title>
           <v-list>
             <v-list-item>
@@ -133,7 +177,7 @@
                 <v-icon>mdi-account</v-icon>
               </v-list-item-action>
               <v-list-item-content>
-                <v-list-item-title>William Tangka</v-list-item-title>
+                <v-list-item-title class="text-h9">William Tangka</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
             <v-divider inset></v-divider>
@@ -142,7 +186,7 @@
                 <v-icon>mdi-phone</v-icon>
               </v-list-item-action>
               <v-list-item-content>
-                <v-list-item-title>(0431) 816-086</v-list-item-title>
+                <v-list-item-title class="text-h9">(0431) 816-086</v-list-item-title>
               </v-list-item-content>
               <v-list-item-action>
                 <v-icon>mdi-message-text</v-icon>
@@ -154,7 +198,7 @@
                 <v-icon>mdi-email</v-icon>
               </v-list-item-action>
               <v-list-item-content>
-                <v-list-item-title>akusayangdia666@gmail.com</v-list-item-title>
+                <v-list-item-title class="text-h9">akusayangdia@gmail.com</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </v-list>
@@ -168,14 +212,26 @@
           >
           </v-img>
           <v-card-title class="red darken-3">
-            <span class="text-h5 white--text">Sekretaris Kelurahan Karame</span>
+            <span class="text-h8 white--text">Sekretaris Lurah</span>
             <v-spacer></v-spacer>
-            <v-btn
-              dark
-              icon
-            >
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
+            <v-menu>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    icon
+                    v-bind="attrs"
+                    v-on="on">
+                    <v-icon color="white">mdi-dots-vertical</v-icon>
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item
+                    v-for="(item, i) in mock.menu"
+                    :key="i"
+                    @click="() => {}">
+                    <v-list-item-title>{{ item }}</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
           </v-card-title>
           <v-list>
             <v-list-item>
@@ -183,7 +239,7 @@
                 <v-icon>mdi-account</v-icon>
               </v-list-item-action>
               <v-list-item-content>
-                <v-list-item-title>Merlin Blonteng</v-list-item-title>
+                <v-list-item-title class="text-h9">Merlin Blonteng</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
             <v-divider inset></v-divider>
@@ -192,7 +248,7 @@
                 <v-icon>mdi-phone</v-icon>
               </v-list-item-action>
               <v-list-item-content>
-                <v-list-item-title>(0431) 816-086</v-list-item-title>
+                <v-list-item-title class="text-h9">(0431) 816-086</v-list-item-title>
               </v-list-item-content>
               <v-list-item-action>
                 <v-icon>mdi-message-text</v-icon>
@@ -204,7 +260,7 @@
                 <v-icon>mdi-email</v-icon>
               </v-list-item-action>
               <v-list-item-content>
-                <v-list-item-title>akusayangdia666@gmail.com</v-list-item-title>
+                <v-list-item-title class="text-h9">akusayangdia@gmail.com</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </v-list>
