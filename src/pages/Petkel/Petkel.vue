869f40703d5137460d7d08743ef15a40c4de692a
Toratomi <tomitodingan18@gmail.com>
2021-08-20 13:52:49 +0800

    perubahan

--- src/pages/Petkel/Petkel.vue
+++ src/pages/Petkel/Petkel.vue
@@ -13,26 +13,26 @@
     </div> -->
     <div>
       <v-card>
-    <v-toolbar
-      dense
-      floating
-    >
-      <v-text-field
-        hide-details
-        prepend-icon="mdi-magnify"
-        single-line
-      ></v-text-field>
+        <v-toolbar
+          dense
+          floating
+        >
+          <v-text-field
+          hide-details
+          prepend-icon="mdi-magnify"
+          single-line
+          ></v-text-field>
 
-      <v-btn icon>
-        <v-icon>mdi-crosshairs-gps</v-icon>
-      </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-crosshairs-gps</v-icon>
+          </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
-  </v-card>
-        <div id="map" style="width: 100%; height: 750px"></div>
+          <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </v-card>
+      <div id="map" style="width: 100%; height: 750px"></div>
     </div>
     
 
